ad66fc89a65983c7d875cc92e53690145eef41ed
sredamar <92477629+sredamar@users.noreply.github.com>
2021-12-16 21:08:34 +0300

    Update itc_rev_par_list.cpp

--- itc_rev_par_list.cpp
+++ itc_rev_par_list.cpp
@@ -1,8 +1,10 @@
 #include "easy_list.h"
-void itc_rev_par_list(vector <int> &mass) {
-    if (mass.size() != 0) {
-    int rp;
-        for (int i = 1; i < mass.size() - 1; i += 2) {
-            rp = mass[i];
-            mass[i] = mass[i + 1];
-            mass[i + 1] = rp;}}}
+void itc_rev_par_list(vector <int> &mass){
+    if (mass.size() > 0){
+    int rev = 0;
+    for (int i = 1 ; i < mass.size(); i += 2){
+        rev = mass[i];
+        mass[i] = mass[i - 1];
+        mass[i - 1] = rev;}
+    }
+}
