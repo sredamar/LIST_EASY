2db52565d646708d6c17b35febd0c9fe1a89521f
sredamar <92477629+sredamar@users.noreply.github.com>
2021-12-16 20:55:36 +0300

    Update itc_rev_par_list.cpp

--- itc_rev_par_list.cpp
+++ itc_rev_par_list.cpp
@@ -2,7 +2,7 @@
 void itc_rev_par_list(vector <int> &mass) {
     if (mass.size() != 0) {
     int rp;
-        for (int i = 1; i < mass.size(); i += 2) {
+        for (int i = 1; i < mass.size() - 1; i += 2) {
             rp = mass[i];
             mass[i] = mass[i + 1];
             mass[i + 1] = rp;}}}
