1662f7e22c6d586eb400cbb4b0eeccd4c50507ef
sredamar <92477629+sredamar@users.noreply.github.com>
2021-12-16 20:52:51 +0300

    Update itc_positive_list.cpp

--- itc_positive_list.cpp
+++ itc_positive_list.cpp
@@ -1,5 +1,4 @@
 #include "easy_list.h"
-#include "easy_list.h"
 int itc_positive_list(const vector <int> &mass) {
     int counter = 0;
     if (mass.size() != 0) for(int i = 0; i < mass.size(); i++) if(mass[i] >= 0) counter += 1;
